d2f02aeebdc98beabda119308b85f6b67cc009b5
Natalya.Selyunina <133572779+NatashaSelyunina@users.noreply.github.com>
2024-06-04 13:32:01 +0300

    Create RoleDto.java

---
+++ backend/src/main/java/reConstructor/domain/dto/RoleDto.java
@@ -0,0 +1,50 @@
+package reConstructor.domain.dto;
+
+import java.util.Objects;
+
+public class RoleDto {
+
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        RoleDto roleDto = (RoleDto) o;
+        return id == roleDto.id && Objects.equals(name, roleDto.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "RoleDto{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
