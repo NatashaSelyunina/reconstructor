311492b57b11f0ba3666538fbadf021b35803953
Natalya.Selyunina <133572779+NatashaSelyunina@users.noreply.github.com>
2024-06-04 21:26:39 +0300

    Create Chef.jsx

---
+++ frontend/src/pages/chef/Chef.jsx
@@ -0,0 +1,60 @@
+import React, { useEffect } from "react";
+import { useTranslation } from "react-i18next";
+import { useNavigate, useParams } from "react-router-dom";
+import styles from "./Chef.module.css";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { readRestaurantById } from "../../redux/actions/restaurantsActions";
+import { readMenuRestaurant } from "../../redux/actions/restaurantsMenuActions";
+import ReadRestaurantHeader from "../../components/readRestaurantHeader/ReadRestaurantHeader";
+import { Button } from "react-bootstrap";
+
+const Chef = () => {
+  const { t } = useTranslation("translation");
+
+  const restaurantId = useSelector((state) => state.user.restaurantId);
+  const { restaurants } = useSelector((state) => state.restaurants);
+  const user = useSelector((state) => state.user.user);
+
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { id } = useParams();
+
+  useEffect(() => {
+    if (restaurantId) dispatch(readRestaurantById({restaurantId}));
+    console.log("restaurant Id in Administrator: ", restaurantId);
+  }, [dispatch, restaurants, restaurantId]);
+
+  useEffect(() => {
+    dispatch(readMenuRestaurant({ restaurantId: id }));
+  }, [dispatch, id, restaurantId]);
+
+  const handleClickPath = (path) => {
+    navigate(path);
+  };
+
+  if (!user) {
+    return navigate("/");
+  }
+
+
+  return (
+    <>
+     <ReadRestaurantHeader />
+      <div className="chefContainer">
+        
+        
+      </div>
+      <div className={styles.container}>
+        <div className={styles.button}>
+        <Button onClick={() => handleClickPath(`/${restaurantId}/menu`)}>
+            {t("createOrder")}
+          </Button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Chef;
