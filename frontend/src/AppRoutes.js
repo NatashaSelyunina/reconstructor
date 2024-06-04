ad3237490a7374118be95de9c6668da3df6d21e6
Natalya.Selyunina <133572779+NatashaSelyunina@users.noreply.github.com>
2024-06-04 21:48:50 +0300

    Create AppRoutes.js

---
+++ frontend/src/AppRoutes.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import routes from "./routes";
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      {routes.map((route, index) => (
+        <Route key={index} path={route.path} element={route.element} />
+      ))}
+
+    </Routes>
+  );
+};
+
+export default AppRoutes;
