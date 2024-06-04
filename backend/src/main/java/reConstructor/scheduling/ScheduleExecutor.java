8e85dd160dc56557e8c9ee5acf6c0b85cd1e00e0
Natalya.Selyunina <133572779+NatashaSelyunina@users.noreply.github.com>
2024-06-04 14:07:10 +0300

    Create ScheduleExecutor.java

---
+++ backend/src/main/java/reConstructor/scheduling/ScheduleExecutor.java
@@ -0,0 +1,10 @@
+package reConstructor.scheduling;
+
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableScheduling
+public class ScheduleExecutor {
+
+}
