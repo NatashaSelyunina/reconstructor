1af856f39dd91895b653186d769b6a7d8126c674
Natalya.Selyunina <133572779+NatashaSelyunina@users.noreply.github.com>
2024-06-04 12:56:59 +0300

    Create MailConfig.java

---
+++ backend/src/main/java/reConstructor/configs/MailConfig.java
@@ -0,0 +1,54 @@
+package reConstructor.configs;
+
+import freemarker.cache.ClassTemplateLoader;
+import freemarker.template.Configuration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@org.springframework.context.annotation.Configuration
+public class MailConfig {
+    @Value("${spring.mail.host}")
+    private String host;
+
+    @Value("${spring.mail.username}")
+    private String username;
+
+    @Value("${spring.mail.password}")
+    private String password;
+
+    @Value("${spring.mail.port}")
+    private int port;
+
+    @Value("${spring.mail.protocol}")
+    private String protocol;
+
+    @Bean
+    public Configuration freeMarkerConfiguration() {
+        Configuration configuration = new Configuration(Configuration.VERSION_2_3_20);
+        configuration.setDefaultEncoding("UTF-8");
+        configuration.setTemplateLoader(new ClassTemplateLoader(MailConfig.class, "/mails/"));
+        return configuration;
+    }
+
+    @Bean
+    public JavaMailSender getMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+
+        mailSender.setHost(host);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
+        mailSender.setPort(port);
+        mailSender.setProtocol(protocol);
+
+        Properties properties = mailSender.getJavaMailProperties();
+        properties.setProperty("mail.smtp.auth", "true");
+        properties.setProperty("mail.smtp.starttls.enable", "true");
+        properties.setProperty("mail.smtp.ssl.trust", host);
+
+        return mailSender;
+    }
+}
