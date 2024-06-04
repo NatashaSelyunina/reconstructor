3b5044938e1d2791bb90e11388e8d3bd52dc2a4d
Natalya.Selyunina <133572779+NatashaSelyunina@users.noreply.github.com>
2024-06-04 19:48:46 +0300

    Create JsonNullableMappingImpl.java

---
+++ backend/target/generated-sources/annotations/reConstructor/services/mapping/JsonNullableMappingImpl.java
@@ -0,0 +1,13 @@
+package reConstructor.services.mapping;
+
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-05-04T00:27:38+0300",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.2 (Oracle Corporation)"
+)
+@Component
+public class JsonNullableMappingImpl extends JsonNullableMapping {
+}
